d61ebb22e0fefa79311b0c7bb32f5527a8723c79
Angelica E. <53525787+angelyka@users.noreply.github.com>
2025-09-18 22:31:54 +0800

    Update streamlit_app.py
    indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     
     
-    time_to_insert = st.button('Submit Order')
+  time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
