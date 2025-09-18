7a8ee25a87c11384d4c94b80f06abb91c60623e7
Angelica E. <53525787+angelyka@users.noreply.github.com>
2025-09-18 22:29:31 +0800

    Update streamlit_app.py
    formatting/indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -26,11 +26,10 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
+  ingredients_string = ''
 
-    ingredients_string = ''
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '
 
     #st.write(ingredients_string)
 
@@ -48,7 +47,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-import requests
+
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
